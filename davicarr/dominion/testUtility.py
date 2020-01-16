7d0b80e5f30fe0c5262643c9b41763eb45959e47
Carrie Davis <carrie7davis@gmail.com>
2020-01-15 17:39:39 -0800

    done with creating testutilities and setting up the placement of def/functions

---
+++ davicarr/dominion/testUtility.py
@@ -0,0 +1,25 @@
+
+
+class Supply():
+    def __init__(self):
+    def getSupplyOrder(self):
+
+    def getSupply(self):
+
+
+class Box():
+    def __init__(self):
+        self.num = num
+
+    def getBox(self):
+        #gets box data
+
+#standalone scripts
+
+def getSupplies():
+
+
+def getSupplyOrders():
+
+
+def getBoxes():
